864e2b5e5a66f39d2d90f3196ca861291598a6f9
Driss Elaloui <drissalaoui040@gmail.com>
2025-10-15 09:22:29 +0100

    Last Version 5

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/xxxx/'  // <-- correspond exactement à ton repo
+  base: '/New-Portfolio/', // CRITIQUE pour GitHub Pages
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  }
 })
